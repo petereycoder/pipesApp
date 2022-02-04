9a28cc94fe73edc745c7f2bc4230f4e194de4f5f
petereycoder <fp.pipe.reyes@gmail.com>
2022-02-03 19:46:37 -0600

    Pipes personalizados en Angular

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+
+@Pipe({
+    name: 'mayusculas'
+})
+export class MayusculasPipe implements PipeTransform {
+
+    transform(valor: string, enMayusculas: boolean = true): string {
+        return (enMayusculas) 
+            ? valor.toUpperCase() 
+            : valor.toLocaleLowerCase();
+    }
+
+}
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -8,15 +8,24 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { VuelaPipe } from './pipes/vuelas.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
+
 
 
 
 @NgModule({
   declarations: [
+    //Componentes
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+    //Pipes
+    MayusculasPipe,
+    VuelaPipe,
+    OrdenarPipe
   ],exports: [
     NumerosComponent,
     NoComunesComponent,
